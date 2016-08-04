9a45ceec4a6fe06c77ed5a58409322fc1a3e9d05
andela-ijubril <issa.jubril@andela.com>
2016-08-04 01:08:16 +0100

    Update the test and add the lists app

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -11,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new to-do app she goes to check
         # out its homepage
@@ -18,28 +24,42 @@ class NewVisitorTest(unittest.TestCase):
 
         # she notices the page title and header mention to-do lists
         self.assertIn('To-DO', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter  to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
+        # she types 'Buy peacock feathers' into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        # When she hits enter, the page updates and the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # There is still a text box inviting her to add another item, She
+        # enters "Use peacock to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith wonders whethe the site will remember her list. The n she sees
+        # that the site has generated a uniwue URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL her to-do list is still there
+
+         # Satisified she goes back to sleep
         self.fail('Finish the test!')
-
-    # She is invited to enter  to-do item straight away
-
-    # she types 'Buy peacock feathers' into a text box (Edith's hobby
-    # is tying fly-fishing lures)
-
-     # When she hits enter, the page updates and the page lists
-     # "1: Buy peacokc feathers" as an item in a to-do list
-
-     # There is still a text box inviting her to add another item, She
-     # enters "Use peacock to make a fly" (Edith is very methodical)
-
-     # The page updates again, and now shows both items on her list
-
-     # Edith wonders whethe the site will remember her list. The n she sees
-     # that the site has generated a uniwue URL for her -- there is some
-     # explanatory text to that effect.
-
-     # She visits that URL her to-do list is still there
-
-     # Satisified she goes back to sleep
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
